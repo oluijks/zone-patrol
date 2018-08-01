46f7141f7411624af5dbe711e6622f184e27a004
Olaf Luijks <oluijks@Olafs-MacBook-Pro.local>
2018-08-01 10:28:59 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zone-patrol-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'zone-patrol';
+}
